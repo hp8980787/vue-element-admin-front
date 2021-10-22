e7aec3b66f19228a6453318d3c2a2055b905fa54
constantine <643711690@qq.com>
2021-10-22 18:00:27 +0800

    订单详情

--- src/views/orders/detail.vue
+++ src/views/orders/detail.vue
@@ -1,46 +1,86 @@
 <template>
   <el-row v-loading="loading">
-    <el-col :span="24">
+    <!-- <el-col :span="24">
       <div class="Echarts">
         <div id="main" style="width: 100%; height: 400px"></div>
       </div>
+    </el-col> -->
+    <el-col :span="24">
+      <el-table :data="data" border style="width: 100%">
+        <el-table-column
+          v-bind:fixed="column === time_column"
+          :width="column === time_column ? '200px' : '100px'"
+          v-for="(column, key) in columns"
+          :key="key"
+          :label="column"
+          :prop="column"
+        ></el-table-column>
+      </el-table>
+    </el-col>
+    <el-col :span="24" style="margin-top: 20px">
+      <el-pagination background layout="prev, pager, next" :total="1000">
+      </el-pagination>
     </el-col>
   </el-row>
 </template>
 
 <script>
-import request from '@/utils/request'
+import request from "@/utils/request";
 export default {
   name: "OrdersDetail",
-  props:["id"],
+  props: ["id"],
   data() {
     return {
       chart: null,
-      loading:true,
-      data:[],
+      loading: true,
+      columns: [],
+      data: [],
+      time_column: "",
+      time_columns: ["dd_time", "created_at", "time"],
     };
   },
   mounted() {
     // this.EchartsInit();
+
+    // this.getColumn();
     this.getOrdersList();
   },
   methods: {
-     getOrdersList() {
-         request({
+    getOrdersList() {
+      request({
         url: `order-database`,
         method: "get",
         params: {
           id: this.id,
         },
-      timeout: 40 * 1000,
+        timeout: 40 * 1000,
       })
         .then((res) => {
-          console.log(res);
           this.loading = false;
+          this.data = res.data.data;
+          for (let i in this.data[0]) {
+            if (this.time_columns.indexOf(i)!=-1) {
+              this.time_column = i;
+            }
+            this.columns.push(i);
+          }
+
+          console.log(this.time_column, this.columns);
         })
         .catch((error) => {
-             this.loading = false;
+          this.loading = false;
         });
+    },
+    async getColumn() {
+      const { data } = await request.get(`/tables/${this.id}`);
+      // if (data.display_column == null) {
+      this.columns = data.full_column;
+      this.time_column = data.time_column;
+      console.log(this.time_column);
+      // } else {
+      // this.columns = data.display_column;
+      // }
+      console.log(data); // this.loading = false;
     },
     EchartsInit() {
       let myChart = this.$echarts.init(document.getElementById("main"));
